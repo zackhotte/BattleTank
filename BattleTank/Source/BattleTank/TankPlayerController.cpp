a5b0cf74cc49fce1fd065705515706a03245826d
zackhotte <zack.hotte@gmail.com>
2019-07-28 11:41:37 -0400

    Added BeginPlay

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,9 +3,21 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* Tank = GetControlledTank();
+	if (Tank != nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayController possessing %s"), *Tank->GetName())
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayController not possessing tank"))
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
-
-
